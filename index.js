cc40606ebeba78e6bcc74fe7ae1d7b02977b3918
kaiikaique685-cmd <kaiikaique685@gmail.com>
2026-03-17 15:52:12 -0300

    Enhance Puppeteer configuration in index.js
    Updated Puppeteer options for Chrome executable path and added additional arguments.

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
+        // No Railway com Nixpacks, o caminho é esse:
         executablePath: '/usr/bin/google-chrome',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--no-zygote'
+        ]
     }
 });
 
 client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+    // Isso vai desenhar o QR Code no log do Railway
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
