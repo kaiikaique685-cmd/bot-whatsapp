836a713ba5939abc28bceb2330b5e403b96d0709
kaiikaique685-cmd <kaiikaique685@gmail.com>
2026-03-17 06:25:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const fs = require('fs');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        executablePath: '/usr/bin/google-chrome',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--no-zygote'
+        ]
+    }
+});
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('✅ Bot online!');
+});
+
+client.initialize();
+
+// --- CONFIGURAÇÕES ---
+const MEU_NUMERO = '5511939595166@c.us';
+const NOME_BOT = 'Nephor';
+
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
